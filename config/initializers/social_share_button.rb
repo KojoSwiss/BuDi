76a2702d11516a255afe3cf534f1c4359aaeb940
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-01-19 15:37:49 -0500

    social_share_button removed

--- config/initializers/social_share_button.rb
+++
@@ -1,5 +0,0 @@
-SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook
-                         email linkedin
-                         telegram whatsapp_app whatsapp_web)
-end
