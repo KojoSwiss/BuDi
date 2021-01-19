e182479e86b1ada10cc729214d8a7403b6c1ebfa
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-01-19 09:20:05 -0500

    Show page modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,14 @@ puts 'Cleaning the database'
 
 Task.destroy_all
 
-200.times do
+10.times do
   task = Task.new(
     title: Faker::Company.profession,
-    description: Faker::Lorem.paragraphs(rand(2..8)).join('\n'),
+    description: Faker::Lorem.paragraphs,
     company: Faker::Movies::LordOfTheRings.character,
     url: Faker::Internet.url,
     category_id: rand(1..6),
-    user_id: 1,
+    user_id: rand(1..7),
     location: Faker::Address.city
     )
 
@@ -19,3 +19,16 @@ Task.destroy_all
   task.save!
 end
 puts 'Done'
+
+puts 'Creating users'
+
+10.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: "123456",
+    )
+    puts "#{user.email} created"
+    user.save!
+end
+
+puts 'Users created'
