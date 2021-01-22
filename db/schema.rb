69bed5f1f13606699f4aed69e409c59d2d2ecf37
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-01-22 11:32:30 -0500

    Geocoded Gem added, Sort by Likes for Categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_230247) do
+ActiveRecord::Schema.define(version: 2021_01_22_162128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 2021_01_18_230247) do
     t.string "location"
     t.string "Ig_url"
     t.string "Tw_url"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
+    t.integer "cached_votes_total", default: 0
   end
 
   create_table "users", force: :cascade do |t|
