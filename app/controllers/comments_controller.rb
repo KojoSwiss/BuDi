18911b05d16c5e61065e27dcc9cdc2e2fbe0f436
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-04-07 12:20:16 +0200

    comment create action added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.create(comment_params)
+    @comment.user_id = current_user.user_id
+
+    if @comment.save
+      redirect_to_task(@task)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:content).permit(:content)
+  end
 end
