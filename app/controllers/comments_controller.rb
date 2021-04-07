caecfc2049a16f31ddc0e583d3c409a14a2936e5
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-04-07 14:00:34 +0200

    Edit / Delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :find_task, only: [:create, :edit, :update, :destroy]
+  before_action :find_comment, only: [:edit, :update, :destroy]
   def create
-    @task = Task.find(params[:task_id])
     @comment = @task.comments.create(comment_params)
     @comment.user_id = current_user.id
 
@@ -11,9 +12,33 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to task_path(@task)
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def find_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def find_comment
+    @comment = @task.comments.find(params[:id])
   end
 end
