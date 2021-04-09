2cbba2ff977247cef5f52b9105f5215b7c57f805
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-04-09 12:37:32 +0200

    Tasks counter added

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_one_attached :photo
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
